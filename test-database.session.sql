4abfc7ed6a9131eb1c36cb5604afd23832e5f1ec
Bevzyk SOI <bevzyk.soi@gmail.com>
2023-01-31 17:35:47 +0100

    Completed 31.01

--- test-database.session.sql
+++ test-database.session.sql
@@ -1,11 +1,13 @@
 DROP TABLE IF EXISTS "Post";
 DROP TABLE IF EXISTS "User";
+
 CREATE TABLE "User" (
     id SERIAL PRIMARY KEY,
     username VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
     is_online BOOLEAN NOT NULL DEFAULT TRUE
 );
+
 CREATE TABLE "Post" (
     id SERIAL PRIMARY KEY,
     body TEXT NOT NULL,
@@ -13,16 +15,18 @@ CREATE TABLE "Post" (
     CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES "User"(id) ON DELETE
     SET NULL
 );
+
 INSERT INTO "User" (username, password)
 VALUES ('test-user', 'test-password'),
     ('test-user-2', 'test-password-2'),
     ('bevzyk-ua', 'Abrakadabra2802');
+
 INSERT INTO "Post" (owner_id, body)
 VALUES (1, 'HELLO, SQL!');
-DELETE FROM "User"
-WHERE id = 1;
+
 SELECT *
 FROM "User";
+
 SELECT "Post".id,
     "Post".body,
     "Post".owner_id,
@@ -30,3 +34,8 @@ SELECT "Post".id,
     "User".is_online
 FROM "Post"
     LEFT JOIN "User" ON "Post".owner_id = "User".id;
+
+DELETE FROM "User"
+WHERE id = 1;
+
+SELECT * FROM User;
